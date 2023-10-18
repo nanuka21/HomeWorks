912b6eb3dc22b705a75ffd864e491786bae46239
nanuka21 <71572530+nanuka21@users.noreply.github.com>
2023-10-18 23:32:45 +0400

    Matrix

---
+++ Matrix.cs
@@ -0,0 +1,8 @@
+﻿
+
+int[,] Matrix = new int[2, 2] { { 1, 3 }, { 4, 5 } };
+Console.WriteLine(Matrix[0, 0] * Matrix[1, 1] - Matrix[0, 1] * Matrix[1, 0]);
+foreach (int number in Matrix)
+{
+    Console.WriteLine(number);
+}
