1b59d5e601a12f0a5b54146c85e334df63392b83
nanuka21 <71572530+nanuka21@users.noreply.github.com>
2023-10-19 22:19:41 +0400

    array sorting 2.2

---
+++ array sorting 2.2task.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 9, 3, 5, 7, 1 };
+int temp = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = i+1; j < array.Length; j++)
+    {
+        if (array[i] > array[j])
+        {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+    Console.WriteLine(array[i]);
+}
