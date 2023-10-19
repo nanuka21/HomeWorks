511ad717cc40ddba22a92e09ba5f821122d52941
nanuka21 <71572530+nanuka21@users.noreply.github.com>
2023-10-19 08:13:04 +0400

    Task1.Game

---
+++ Game.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Hello this is Number Guessing Game you have 3 Attemps , good luck");
+int attemps = 0;
+int maxattemps = 3;
+bool won = false;
+
+
+Random random = new Random();
+int randomnumber = random.Next(0, 10);
+
+while (attemps<maxattemps)
+{
+    Console.WriteLine("Enter th number");
+    int customernumber = int.Parse(Console.ReadLine());
+
+    if (customernumber == randomnumber)
+    {
+        Console.WriteLine("Congretulation you Won");
+        break;
+    }
+
+    if (customernumber!=randomnumber)
+        {
+            Console.WriteLine("Your Number is not Correct");
+       
+        }
+   
+    attemps++;
+     
+}
+if (!won)
+{
+    Console.WriteLine("Sorry you Lose");
+}
