25f7e79583e04d72fab28b9634d1f94309329f04
nanuka21 <71572530+nanuka21@users.noreply.github.com>
2023-10-20 12:47:23 +0400

    Task3.1Method

---
+++ Methodindex.cs
@@ -0,0 +1,43 @@
+﻿
+
+class program
+{
+    static void Main()
+    {
+
+        int[] myarray = { 11, 12, 13, };
+        int sum = SumofIndex(myarray, 1);
+        Console.WriteLine(sum);
+
+
+    }
+
+
+
+    static int SumofIndex(int[] array, int index)
+
+    {
+        int sum = 0;
+
+        int number = array[index];
+
+        string numbertostring = number.ToString();
+
+        for (int i = 0; i < numbertostring.Length; i++)
+        {
+            char chardigit = numbertostring[i];
+            if (char.IsDigit(chardigit))
+            {
+                int digit = int.Parse(chardigit.ToString());
+                sum += digit;
+            }
+        }
+        return sum;
+
+    }
+
+
+
+
+
+}
