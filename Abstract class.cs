4343fcbda1070eeb05b178ada7e62c40a6744f51
nanuka21 <71572530+nanuka21@users.noreply.github.com>
2023-10-28 15:03:33 +0400

    Abstract class Homework
    Abstract Class Homework

---
+++ Abstract class.cs
@@ -0,0 +1,83 @@
+﻿namespace Abstraact_CLass
+{
+
+    class program
+    {
+        static void Main(String [] args)
+        {
+
+            Console.WriteLine("Choose a shape");
+            Console.WriteLine("Triangle");
+            Console.WriteLine("Rectangle");
+            Console.WriteLine("Circle");
+
+            int choice = int.Parse(Console.ReadLine());
+
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter the sides of Triangle (a , b, c");
+                    double a = double.Parse(Console.ReadLine());
+                    double b = double.Parse(Console.ReadLine());
+                    double c = double.Parse(Console.ReadLine());
+                    Triangle triangle = new Triangle(a, b, c);
+                    Console.WriteLine("Area is:" + triangle.Area);
+                    Console.WriteLine("Perimeter is :" + triangle.perimeter);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the width end height of Rectangle");
+                    double height = double.Parse(Console.ReadLine());
+                    double width = double.Parse(Console.ReadLine());
+                    Rectangle rectangle = new Rectangle(height, width);
+                    Console.WriteLine("area is " + rectangle.Area);
+                    Console.WriteLine("Perimeter is" + rectangle.perimeter);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter the radius");
+                    double radius = double.Parse(Console.ReadLine());
+                    Circle circle = new Circle(radius);
+                    Console.WriteLine("are is " + circle.Area);
+                    Console.WriteLine("perimeter is " + circle.perimeter);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+
+
+
+
+
+
+
+            }
+           
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ Shape.cs
@@ -0,0 +1,93 @@
+﻿using System;
+namespace Abstraact_CLass
+{
+	public abstract class shape
+	{
+
+		public abstract double Area();
+		public abstract double perimeter();
+	}
+
+
+	public class Triangle : shape
+	{
+		private double a, b, c;
+
+		public Triangle(double a, double b, double c)
+		{
+			if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a)
+			{
+				throw new ArgumentException("invalid data");
+			}
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
+
+		public override double Area()
+		{
+			double s = (a + b + c) / 2.0;
+			return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+		}
+
+		public override double perimeter()
+		{
+			return a + b + c;
+		}
+	}
+	public class Rectangle : shape
+	{
+
+
+		private double width, height;
+
+		public Rectangle(double width, double height)
+		{
+			if (width <= 0 || height <= 0)
+			{
+				throw new ArgumentException("this height is not valid ");
+
+			}
+			this.width = width;
+			this.height = height;
+		}
+
+
+		public override double Area()
+		{
+			return width * height;
+		}
+		public override double perimeter()
+		{
+			return 2 * (width + height);
+		}
+
+	}
+
+	public class Circle : shape
+	{
+
+		private double radius;
+
+		public Circle(double radius)
+		{
+			if (radius <= 0)
+			{
+				throw new ArgumentException("this is invalid circle radius");
+			}
+
+			this.radius = radius;
+		}
+
+		public override double Area()
+		{
+			return Math.PI * radius * radius;
+		}
+		public override double perimeter()
+		{
+			return 2 * Math.PI * radius * radius;
+		}
+
+	}
+	
+}
